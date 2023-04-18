8da8c3b24f2ea34cd0719e41508da10db3bb00a7
zian wang <zianwang623@gmail.com>
2023-04-18 18:35:28 -0400

    update 8

--- src/StringClass/stringTest.java
+++ src/StringClass/stringTest.java
@@ -2,10 +2,44 @@ package StringClass;
 
 public class stringTest {
     public static void main(String[] args) {
-        String s1 = "abc";
-        String s2 = s1.repeat(3);
-        String s3 = s1.replace(s1,"de");
-        System.out.println(s2);
-        System.out.println(s3);
+//        people tom1 = new people();
+//        people tom2 = new people();
+//
+//        tom1.setName(new String("tom"));
+//        tom2.setName(new String("tom"));
+//
+//        System.out.println(tom1.getName() == tom2.getName());
+//        System.out.println(tom1.getName().equals(tom2.getName()));
+
+        String a = "abcdefgcd";
+        System.out.println(a.lastIndexOf("cd"));
+    }
+}
+
+class people{
+    private int age;
+    private String name;
+
+    public people(String name) {
+        this.name = name;
+    }
+
+    public people() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
